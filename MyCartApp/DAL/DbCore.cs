9ce09f5f40297477537b15039f6f0574c9c875b3
singamsiva1993 <singam.sivagangireddy@gmail.com>
2016-05-18 12:38:49 +0530

    first commit

---
+++ MyCartApp/DAL/DbCore.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MyCartApp.DAL
+{
+    public class DbCore
+    {
+        private string constring;
+
+        public DbCore()
+        {
+            constring = ConfigurationManager.ConnectionStrings["mycart"].ToString();
+        }
+
+        public int CoreOps(string proc, SqlParameter[] param)
+        {
+            int res = 0;
+            using (SqlConnection con = new SqlConnection(constring))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand(proc, con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    if (param != null)
+                    {
+                        cmd.Parameters.AddRange(param);
+                    }
+                    res = cmd.ExecuteNonQuery();
+
+                }
+                con.Close();
+            }
+            return res;
+        }
+        public DataTable GetTable(string proc, SqlParameter[] param)
+        {
+            DataTable dt = null;
+            using (SqlConnection con = new SqlConnection(constring))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand(proc, con))
+                {
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddRange(param);
+                    SqlDataAdapter dab = new SqlDataAdapter(cmd);
+                    dt = new DataTable();
+                    dab.Fill(dt);
+                }
+                con.Close();
+            }
+            return dt;
+        }
+        public DataTable GetTable(string proc)
+        {
+            DataTable dt = null;
+            using (SqlConnection con = new SqlConnection(constring))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand(proc, con))
+                {
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    SqlDataAdapter dab = new SqlDataAdapter(cmd);
+                    dt = new DataTable();
+                    dab.Fill(dt);
+                }
+                con.Close();
+            }
+            return dt;
+        }
+    }
+}
---
+++ MyCartApp/WebForm1.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="MyCartApp.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder" runat="server">
+</asp:Content>
